7ead8d5722661803ccf3a7bbbc30945ef9783b98
tuanqing <tuanqing.re@gmail.com>
2019-11-17 14:47:20 +0800

    import 5.3.x kernel from flippy

---
+++ armbian/n1/boot/emmc_autoscript.cmd
@@ -0,0 +1,8 @@
+setenv env_addr 0x10400000
+setenv kernel_addr 0x11000000
+setenv initrd_addr 0x13000000
+setenv dtb_mem_addr 0x1000000
+setenv boot_start booti ${kernel_addr} ${initrd_addr} ${dtb_mem_addr}
+setenv addmac 'if printenv mac; then setenv bootargs ${bootargs} mac=${mac}; elif printenv eth_mac; then setenv bootargs ${bootargs} mac=${eth_mac}; fi'
+if fatload mmc 1 ${kernel_addr} zImage; then if fatload mmc 1 ${initrd_addr} uInitrd; then if fatload mmc 1 ${env_addr} uEnv.ini; then env import -t ${env_addr} ${filesize}; run addmac; fi; if fatload mmc 1 ${dtb_mem_addr} ${dtb_name}; then run boot_start;fi;fi;fi;
+
